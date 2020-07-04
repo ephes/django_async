a74e34e828e95c54dc1f05ed28c6a4f5a572b3fb
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-07-04 07:42:07 +0200

    just route all functions in views ending in 'view' with their name

--- django_async/urls.py
+++ django_async/urls.py
@@ -1,25 +1,8 @@
-"""django_async URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/dev/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
 
-from .views import test_get, test_async_get
+from . import views
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("foobar/", test_get),
-    path("async_foobar/", test_async_get),
-]
+urlpatterns = []  # add all functions ending with "view" to urlpatterns
+for view in dir(views):
+    if view.endswith("view"):
+        urlpatterns.append(path(view + "/", getattr(views, view)))
