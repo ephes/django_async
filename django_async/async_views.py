d87d0ef945d9b68493656a04829ca8d89960d52a
Jochen Wersdörfer <jochen@wersdoerfer.de>
2020-07-05 10:44:40 +0200

    some refactoring + async aggregation view

---
+++ django_async/async_views.py
@@ -0,0 +1,26 @@
+import time
+import httpx
+import asyncio
+
+from django.http import JsonResponse
+from .viewfinder import get_all_functions
+
+
+async def aggregation_of_sync_view(request):
+    # start server like this:
+    # uvicorn --workers 10 django_async.asgi:application
+    print("locals: ", locals())
+    responses = []
+    url = "http://127.0.0.1:8000/sync/api/"
+    urls = [url for _ in range(10)]
+    s = time.perf_counter()
+    async with httpx.AsyncClient() as client:
+        responses = await asyncio.gather(*[client.get(url) for url in urls])
+        responses = [r.json() for r in responses]
+    elapsed = time.perf_counter() - s
+    print(f"fetch executed in {elapsed:0.2f} seconds.")
+    result = {"responses": responses}
+    return JsonResponse(result)
+
+
+all_functions = get_all_functions(globals().copy())
--- django_async/urls.py
+++ django_async/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from . import views
-from . import just_sync
+from . import sync_views
+from . import async_views
 
 def urls_with_prefix(prefix, all_functions):
     return [path(f"{prefix}/{name}/", view) for name, view in all_functions]
 
 urlpatterns = []
 urlpatterns.extend(urls_with_prefix("views", views.all_functions))
-urlpatterns.extend(urls_with_prefix("sync", just_sync.all_functions))
+urlpatterns.extend(urls_with_prefix("sync", sync_views.all_functions))
+urlpatterns.extend(urls_with_prefix("async", async_views.all_functions))
